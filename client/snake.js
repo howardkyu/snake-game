f50cf229faefd61ab7d9dd8a0d7bc3b6ef85c175
Howard Yu <howard.yu.96@gmail.com>
2017-02-03 01:35:44 -0800

    Fixed message errors

--- client/snake.js
+++ client/snake.js
@@ -1,9 +1,8 @@
-var COL = 90;
-var ROW = 60;
 var CELL_PX = 10;
-var PLAYER_ONE_COLOR = 'green';
-var PLAYER_TWO_COLOR = 'red';
-var BG_COLOR = 'white';
+var PLAYER_ONE_COLOR = "green";
+var PLAYER_TWO_COLOR = "red";
+var FOOD_COLOR = "blue";
+var BG_COLOR = "white";
 
 var Server;
 var gameCanvas;
@@ -18,43 +17,57 @@ var d2 = 'L';
 
 var newGame = true;
 
-function getPlayer(id) {
-    if (playerOne.id === id) return playerOne;
-    else return playerTwo;
-}
-
 function receive(message) {
     var messageList = message.split(":");
 
-    console.log(messageList[0])
+    if (messageList[0] === "SETUP") {
 
-    if (messageList[0] === "WELCOME") {
+        updateCanvas(Number(messageList[1]), Number(messageList[2]));
         init();
-    } else if (messageList[0] === "INIT") {
-        for (var i = 2; i < messageList.length; i+=2) {
-            draw({x: messageList[i], y: messageList[i+1]}, getPlayer(messageList[1]).color);
+
+    } else if (messageList[0] === "STATE") {
+
+        for (var i = 1; i < messageList.length; i++) {
+            var state = messageList[i].split(",");
+
+            switch(state[0]) {
+                case("FOOD"):
+                    draw({x: state[1], y: state[2]}, FOOD_COLOR);
+                    console.log(FOOD_COLOR);
+                    break;
+                case("EMPTY"):
+                    erase({x: state[1], y: state[2]});
+                    break;
+                case("PLAYER1"):
+                    drawPlayer({x: state[1], y: state[2]}, playerOne);
+                    break;
+                case("PLAYER2"):
+                    drawPlayer({x: state[1], y: state[2]}, playerTwo);
+                    break;
+                case("SCORE1"):
+                    playerOne.score = state[1];
+                    updateScoreBoard();
+                    break;
+                case("SCORE2"):
+                    playerTwo.score = state[1];
+                    updateScoreBoard();
+                    break;
+            }
         }
-    } else if (messageList[0] === "ERROR") {
-        /* DO NOTHING */
-    } else if (messageList[0] === "READY") {
-        /* DO NOTHING */
-    } else if (messageList[0] === "DRAW") {
-        draw({x: messageList[2], y: messageList[3]}, getPlayer(messageList[1]).color);
-    } else if (messageList[0] === "ERASE") {
-        console.log("ASDFASDF");
-        draw({x: messageList[2], y: messageList[3]}, BG_COLOR);
-    } else if (messageList[0] === "SCORE") {
-        getPlayer(messageList[1]).score = parseInt(messageList[2]);
-        updateScoreBoard();
-    } else if (messageList[0] === "GAME_OVER") {
-        /* GAME OVER */
+
+    } else if (messageList[0] === "NEWGAME") {
+        comparePlayersScore();
+        displayWinner();
     }
 }
 
-function sendDirection(player, direction) {
-    Server.send("MOVE " + player.id + " " + direction);
-}
+function updateCanvas(col, row) {
+    gameCanvas.height = col * CELL_PX;
+    gameCanvas.width = row * CELL_PX;
 
+    scoreCanvas.width = col / 4 * CELL_PX;
+    scoreCanvas.width = row * CELL_PX;
+}
 
 function init() {
     document.addEventListener('keydown', function(event) {
@@ -74,10 +87,7 @@ function init() {
         if (event.keyCode == 32) Server.send("START");
     });
 
-    gameCanvas = document.getElementById("canvas-game");
     gameCtx = gameCanvas.getContext("2d");
-
-    scoreCanvas = document.getElementById("canvas-score");
     scoreCtx = scoreCanvas.getContext("2d");
 
     updateScoreBoard();
@@ -90,12 +100,16 @@ function connect() {
     Server.bind('open', function() {
         // document.getElementById('cnt-btn').disabled = true;
 
+        gameCanvas = document.getElementById("canvas-game");
+        scoreCanvas = document.getElementById("canvas-score");
+
         var playerOneId = document.getElementById('player-1-id').value;
         var playerTwoId = document.getElementById('player-2-id').value;
         playerOne = {id: playerOneId, color: PLAYER_ONE_COLOR, score: 0};
         playerTwo = {id: playerTwoId, color: PLAYER_TWO_COLOR, score: 0};
 
-        Server.send("CONNECT " + playerOne.id + " " + playerTwo.id);
+        Server.send("INIT:" + playerOne.id);
+        Server.send("INIT:" + playerTwo.id);
     });
 
     Server.bind('close', function(data) {
@@ -107,22 +121,61 @@ function connect() {
     Server.connect();
 }
 
+function getPlayer(id) {
+    if (playerOne.id === id) return playerOne;
+    else return playerTwo;
+}
+
+function sendDirection(player, direction) {
+    Server.send("MOVE " + player.id + " " + direction);
+}
+
 function draw(cell, color) {
    gameCtx.fillStyle = color;
    gameCtx.fillRect(cell.x * CELL_PX, cell.y * CELL_PX, CELL_PX, CELL_PX); 
 }
 
+function drawPlayer(cell, player) {
+    draw(cell, player.color);
+}
+
+function erase(cell) {
+   draw(cell, BG_COLOR);
+}
+
 function updateScoreBoard() {
     // If not a new game, then clear the canvas to update the score
     if(!newGame) {
-        scoreCtx.clearRect(0,0,1100,100);
+        scoreCtx.clearRect(0,0,scoreCanvas.width,scoreCanvas.height);
     }
     else {
         newGame = false;
     }
     
     scoreCtx.font = "30px Ariel";
-    scoreCtx.fillText(playerOne.id + "'s Score: " + playerOne.score, 100,30);
-    scoreCtx.fillText(playerTwo.id + "'s Score: " + playerTwo.score, 800,30);
+    scoreCtx.textAlign = "center";
+    scoreCtx.fillText(playerOne.id + "'s Score: " + playerOne.score, scoreCanvas.width*2/8, scoreCanvas.height/2);
+    scoreCtx.fillText(playerTwo.id + "'s Score: " + playerTwo.score, scoreCanvas.width*6/8, scoreCanvas.height/2);
     
+}
+
+function comparePlayersScore() {
+    if(playerOne.score > playerTwo.score) {
+        return playerOne.id.toUpperCase() + " WINS!";
+    }
+    else if (playerOne.score < playerTwo.score) {
+        return playerTwo.id.toUpperCase() + " WINS!";
+    }
+    else {
+        return "IT'S A TIE!";
+    }
+}
+
+function displayWinner(text) {
+    gameCtx.clearRect(0,0,gameCanvas.width,gameCanvas.height);
+    gameCtx.font = "40px Ariel";
+    gameCtx.fillStyle = "red";
+    gameCtx.textAlign = "center";
+    var winner = comparePlayersScore();
+    gameCtx.fillText("GAME OVER: " + winner, gameCanvas.width/2, gameCanvas.height/2);
 }
